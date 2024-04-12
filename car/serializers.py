c14bf029233916fec839db48b80ec4a33abcb0a0
mkuchuman <mkuchuman1@gmail.com>
2024-04-12 21:38:59 +0200

    Solution

--- car/serializers.py
+++ car/serializers.py
@@ -1,5 +1,15 @@
 from rest_framework import serializers
+from car.models import Car
 
 
-class CarSerializer(serializers.Serializer):
-    pass
+class CarSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Car
+        fields = [
+            "id",
+            "manufacturer",
+            "model",
+            "horse_powers",
+            "is_broken",
+            "problem_description",
+        ]
--- main.py
+++ main.py
@@ -1,9 +1,23 @@
+from io import BytesIO
+
+from rest_framework.exceptions import ValidationError
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serialized_car = CarSerializer(car)
+    json = JSONRenderer().render(serialized_car.data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = BytesIO(json)
+    data = JSONParser().parse(stream)
+    serialized_car = CarSerializer(data=data)
+    if serialized_car.is_valid():
+        return serialized_car.save()
+    raise ValidationError(serialized_car.errors)
